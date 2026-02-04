618f15f955b4553411bed7fc6a97c731f71eb133
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-04 14:26:28 +0900

    feat(nvim) create shortcut to toggle nvim-tree.

--- .config/nvim/lua/config/base.lua
+++ .config/nvim/lua/config/base.lua
@@ -31,4 +31,19 @@ function M.DeleteCurrentBuffer()
     vim.api.nvim_buf_delete(0, {force = false})
 end
 
+function M.OpenOrCloseNvimTree()
+    local api = require("nvim-tree.api")
+    if api.tree.is_visible() then
+        local current_window_id = vim.api.nvim_get_current_win()
+        local tree_window_id = api.tree.winid()
+        if current_window_id == tree_window_id then
+            api.tree.close()
+        else
+            api.tree.focus()
+        end
+    else
+        api.tree.open()
+    end
+end
+
 return M
