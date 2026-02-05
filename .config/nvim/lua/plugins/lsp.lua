cb81f8073c0b495602a3cf05dee040f0a716139f
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-06 06:03:12 +0900

    feat(nvim) add lsp gpls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,28 +1,51 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        vim.diagnostic.config({
-            virtual_text = true,
-            underline = true,
-            update_in_insert = false,
-            severity_sort = true,
-            float = {
-                border = "rounded",
-                source = true,
-            },
-
-        })
-
-        vim.lsp.config("lua_ls", {
-            runtime = {
-                version = "LuaJIT",
-            },
-            workspace = {
-                library = {
-                    vim.env.VIMRUNTIME,
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.diagnostic.config({
+                virtual_text = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+                float = {
+                    border = "rounded",
+                    source = true,
                 },
-            },
-        })
-        vim.lsp.enable("lua_ls")
-    end,
+            })
+
+            local servers = {
+                lua_ls = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    workspace = {
+                        library = {
+                            vim.env.VIMRUNTIME,
+                        },
+                    },
+                },
+                gopls = {},
+            }
+
+            for name, config in pairs(servers) do
+                vim.lsp.config(name, config)
+            end
+        end,
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "gopls",
+                },
+            })
+        end
+    }
 }
