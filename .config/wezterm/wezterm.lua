e21eb559d000e154505198fd3aee11f60eeae308
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-05 17:22:39 +0900

    feat(term) create wezterm setup file

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.automatically_reload_config = true
+config.font_size = 13.5
+config.use_ime = true
+
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+
+
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- and finally, return the configuration to wezterm
+return config
