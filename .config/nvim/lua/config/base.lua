ed437a04b8439ed3702bf7a6112e98a2db1a7a88
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-09 20:44:53 +0900

    chore(nvim) add vim.opt setting

--- .config/nvim/lua/config/base.lua
+++ .config/nvim/lua/config/base.lua
@@ -1,10 +1,12 @@
 -- basic
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.autoindent = true
-vim.opt.fillchars = {eob=" "}
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.fillchars = { eob = " " }
+vim.o.wrap = false
 
 -- lazy.nvim
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -22,26 +24,26 @@ vim.g.loaded_netrwPlugin = 1
 local M = {}
 
 function M.ToggleLineNumber()
-    vim.o.number = not(vim.o.number)
+	vim.o.number = not vim.o.number
 end
 
 function M.ToggleRelativeLineNumber()
-    vim.o.relativenumber = not(vim.o.relativenumber)
+	vim.o.relativenumber = not vim.o.relativenumber
 end
 
 function M.OpenOrCloseNvimTree()
-    local api = require("nvim-tree.api")
-    if api.tree.is_visible() then
-        local current_window_id = vim.api.nvim_get_current_win()
-        local tree_window_id = api.tree.winid()
-        if current_window_id == tree_window_id then
-            api.tree.close()
-        else
-            api.tree.focus()
-        end
-    else
-        api.tree.open()
-    end
+	local api = require("nvim-tree.api")
+	if api.tree.is_visible() then
+		local current_window_id = vim.api.nvim_get_current_win()
+		local tree_window_id = api.tree.winid()
+		if current_window_id == tree_window_id then
+			api.tree.close()
+		else
+			api.tree.focus()
+		end
+	else
+		api.tree.open()
+	end
 end
 
 return M
