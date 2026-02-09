11a8ae7b8e1150a82e63b44a4b33e51b75e26160
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-10 05:02:39 +0900

    feat(wezterm) ligature off

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,10 +7,10 @@ config.automatically_reload_config = true
 config.font_size = 13.5
 config.use_ime = true
 
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
 
-
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- This is where you actually apply your config choices
 
