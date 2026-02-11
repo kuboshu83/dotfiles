f52a728c38f43ec2c4210ad8e9f5ae0401140f0f
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-11 09:07:31 +0900

    feat: introduced autopairs and integrated it with cmp.

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -7,3 +7,8 @@ vim.api.nvim_set_hl(0, "@lsp.mod.unused", {
 	italic = true,
 	undercurl = true,
 })
+
+-- completion
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
