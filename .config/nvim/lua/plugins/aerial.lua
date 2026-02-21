e80b5f2a46e1c70494182e766f68c781357046dc
kuboshu83 <dot.sohkubo@gmail.com>
2026-02-21 22:24:28 +0900

    feat: update config outline

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -5,6 +5,13 @@ return {
 			vim.keymap.set("n", "{", "<cmd>AerialPrev<cr>", { buffer = bufnr, noremap = true, silent = true })
 			vim.keymap.set("n", "}", "<cmd>AerialNext<cr>", { buffer = bufnr, noremap = true, silent = true })
 		end,
+		layout = {
+			min_width = 30,
+			default_direction = "left",
+		},
+		show_guides = true,
+		filter_kind = false,
+		highlight_on_jump = 300,
 	},
 	-- Optional dependencies
 	dependencies = {
